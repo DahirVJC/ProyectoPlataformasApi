b8b837a2f397c4008451bf255266167193e86929
DahirVJC <78052629+DahirVJC@users.noreply.github.com>
2024-05-04 15:03:46 -0700

    Generate project

---
+++ src/main/java/com/plataformasHeterogeneas/ProyectoPlataformasApi/ProyectoPlataformasApiApplication.java
@@ -0,0 +1,13 @@
+package com.plataformasHeterogeneas.ProyectoPlataformasApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPlataformasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPlataformasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plataformasHeterogeneas/ProyectoPlataformasApi/ProyectoPlataformasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plataformasHeterogeneas.ProyectoPlataformasApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPlataformasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
